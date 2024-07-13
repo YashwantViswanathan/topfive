c20176124ba865bee2902d86184087389f4537fa
YashwantViswanathan <f20220599@pilani.bits-pilani.ac.in>
2024-07-13 08:40:23 +0530

    Some more comments

--- topfive/settings.py
+++ topfive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q#mrjl&0pp+(%ta82gcw742+o$aa27aco!xp3x0x(^2hlou_!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = [".awsapprunner.com", '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
